53b817a71acf1fe0eeae092ee79f66349a05f45f
ornelle <42754183+ornelle@users.noreply.github.com>
2019-03-20 20:29:55 -0700

    cries

--- sudoku.c
+++ sudoku.c
@@ -5,6 +5,7 @@
 
 int rows = 9;
 int columns = 9;
+bool getAllowedValues(int sudoku[9][9], int row, int col, int number);
 
 void display(int sudoku[9][9]){
 	for (int i = 0; i < rows; i++){
@@ -36,42 +37,42 @@ void addInitialValue(int sudoku[9][9], int r, int c, const int value){
 	sudoku[r][c] = value;
 }
 
-bool fillinBoard(int row, int col){
+void fillinBoard(int sudoku[9][9], int row, int col){
 	/* if attempting the extra credit this creates a randomize sudoku puzzle using recursion */ 
         //^^above will pass in 0,0 to START
-	
-        //BASE CASE: controls recursions
-        if(row>8)
-            return true;
-        //generates new int
-        int randomNum = rand()% 10; // returns a random int between 0-9
+	int randomN;
+	for(int r = 0; r < 9; r++) {
+		for (int c = 0; c < 9; c++) {
+			randomN= (rand() % 9)+1;
+			// srand(time(0));
+			if(getAllowedValues(sudoku, r, c, randomN)){
+				if (randomN!=0)
+					sudoku[r][c]=randomN;
+			}
+			//makeshift while loop: if position is not good go back to it & try again
+			else{
+				randomN=9;
+				//printf("%d\n", randomN );
+				while(!getAllowedValues(sudoku, r, c, randomN)){
+					printf("%d\n valuee\n", randomN);
+					--randomN;
+				}
+				if (randomN>0)
+					sudoku[r][c]=randomN;
+				if (sudoku[r][c]==0){
+					for(int i=0; i<9; i++){
+						if(getAllowedValues(sudoku, r, c, i))
+							sudoku[r][c]=i;
+						display(sudoku);
+					}
+				}		
 
-        //go to next row if your column is full
-        if(col>8)
-            return fillinBoard(row+1, 0);
-        //9 possible attempts
-        for(int i=0; i<9; i++) {
-            if (canDo(row, col, randomNum)) {
-                //if board is empty it'll insert it in if no
-                board[row][col] = randomNum;
-                if (fillinBoard(row, col + 1))
-                    //if returned true it goes back to base case!!!!
-                    return true;
-            }
-            randomNum++;
-            //checks other possible ints at that point on board
-            if(randomNum>9)
-                randomNum=1;
-            /* ^goes back to next for loop and trys all the numbers that came prior to randomNum */
-
-        }
-        //BACK TRACKS: if all attempts at that point has not worked
-        //what false are we returning didnt work at all?
-        board[row][col]=0;
-        return false;
+			}
+		}
 
     }
 
+}
 int checkPuzzle(int r, int c, int guess){
 	int valid = 0;
 	int solved[9][9] = {{1, 7, 3,   5, 2, 9,   8, 6, 4},
@@ -110,7 +111,7 @@ int isStillEmpty(int sudoku[9][9]){
 }
 
 void addGuess(int start[9][9], int sudoku[9][9], int r, int c, int guess){
-	if (start[r][c] == 0){
+	if (sudoku[r][c] == 0){
 		if(isStillEmpty(sudoku)){
 			if(checkPuzzle(r, c, guess)){
 				sudoku[r][c] = guess;
@@ -124,23 +125,22 @@ void addGuess(int start[9][9], int sudoku[9][9], int r, int c, int guess){
 }
 
 
-bool getAllowedValues(int sudoku[9][9], int r, int c, int number){
+bool getAllowedValues(int sudoku[9][9], int row, int col, int number){
 
 	//returns numbers that are allowed in the chosen square
-	bool fal= false;
-  	bool x= true;
-	//returns numbers that are allowed in the chosen square? 
+	//bool fal= false;
+  	//bool x= true;
 
 	//checks row,
   	for(int i=0; i<9; i++){
-    		if(sudoku[row][i]==number)
-      	return fal;
+    	if(sudoku[row][i]==number)
+      		return false;
   	}
   
   	//checks column, 
   	for(int i=0; i<9; i++){
-    		if (sudoku[i][col]==number)
-      		return fal;
+    	if (sudoku[i][col]==number)
+      		return false;
   	}
 
   	//checks box
@@ -148,20 +148,24 @@ bool getAllowedValues(int sudoku[9][9], int r, int c, int number){
   	int startC = col- (col % 3);
     	//actual logic for checking
   	for( int co= startC; co<startC+3; co++) {
-    		for (int ro = startR; ro <startR + 3; ro++) {
-      			if (sudoku[ro][co] == number) {
-          			return fal;
-          			}		
-      			}
+    	for (int ro = startR; ro <startR + 3; ro++) {
+      		if (sudoku[ro][co] == number) {
+          		return false;
+          		}		
+      		}
     	}
-  	return x;
+  	return true;
 }
 
  void reset(int start[9][9], int sudoku[9][9]){
-
-	createEmptyBoard(start);
+ 	//use pointers
+ 	int count=0;
+	//createEmptyBoard(sudoku);
 	for (int i = 0; i < rows; i++){
 		for (int j = 0; j < columns; j++){
+			count++;
+			if (count%5!=0)
+				sudoku[i][j]=0;
 			if(sudoku[i][j] != 0)
 				addInitialValue(start, i, j, 1);
 		}
@@ -170,37 +174,43 @@ bool getAllowedValues(int sudoku[9][9], int r, int c, int number){
 }
 
 int main(){
-	int empty, choice;
+	int keepPlaying=0;
+	int choice;
 	int userRow, userCol, userValue;
-	
-	srand(time(0)); //seed for random generator
 	int start[9][9];
-	int sudoku[9][9] = {{0, 7, 3,   0, 0, 0,   8, 0, 0},
-						{0, 0, 4,   1, 3, 0,   0, 5, 0},
-						{0, 8, 5,   0, 0, 6,   3, 1, 0},
-						{5, 0, 0,   0, 9, 0,   0, 3, 0},
-						{0, 0, 8,   0, 1, 0,   5, 0, 0},
-						{0, 1, 0,   0, 6, 0,   0, 0, 7},
-						{0, 5, 1,   6, 0, 0,   2, 8, 0},
-						{0, 4, 0,   0, 5, 2,   9, 0, 0},
-						{0, 0, 2,   0, 0, 0,   6, 4, 0}};
+	srand(time(0)); //seed for random generator
+	int sudoku[9][9];
 
-	printf("Welcome to Sudoku!\nHere is your game: \n");
-	createEmptyBoard(start);
-	for (int i = 0; i < rows; i++){
-		for (int j = 0; j < columns; j++){
-			if(sudoku[i][j] != 0)
-				addInitialValue(start, i, j, 1);
+	createEmptyBoard(sudoku);
+	fillinBoard(sudoku, 0, 0);
+	printf("does it work\n");
+
+	/* creates boolean board */
+	for(int i=0; i< rows; i++){
+		for(int j=0; j< columns; j++){
+			start[i][j]= 1;
 		}
 	}
+
+	//prints initial board
+	printf("Welcome to Sudoku!\nHere is your game: \n");
 	display(sudoku);
+	reset(start, sudoku);
+
+	// for (int i = 0; i < rows; i++){
+	// 	for (int j = 0; j < columns; j++){
+	// 		if(sudoku[i][j] != 0)
+	// 			addInitialValue(sudoku, i, j, 1);
+	// 	}
+	// }
 
 
-	while (empty == 0){
+	while (keepPlaying == 0){
 		printf("\nWhat would you like to do?\n(1) Add a guess\n"
 			"(2) Get a hint\n(3) Display the current board\n(4) Reset "
 			"the game\n(5) Exit\n");
 		scanf("%d", &choice);
+		printf("choice is %d\n", choice);
 
 		switch(choice){
 			case 1:
@@ -209,14 +219,15 @@ int main(){
 				addGuess(start, sudoku, userRow-1, userCol-1, userValue);
 				display(sudoku);
 				if (!isStillEmpty(sudoku)){
-					empty++;
+					keepPlaying++;
 				}
 				break;
 			case 2:
 				printf("\nEnter a row and column:\n");
 				scanf("%d %d", &userRow, &userCol);
 				printf("\nThese are your options: \n");
-				getAllowedValues(sudoku, userRow, userCol);
+				int numb=0; //fix this
+				getAllowedValues(sudoku, userRow, userCol, numb);
 				break;
 			case 3:
 				printf("\nHere is your current board:\n");
@@ -224,18 +235,18 @@ int main(){
 				break;
 			case 4:
 				printf("\nResetting the game...\n");
-				createEmptyBoard(start);
+				createEmptyBoard(sudoku);
 					for (int i = 0; i < rows; i++){
 						for (int j = 0; j < columns; j++){
 							if(sudoku[i][j] != 0)
-								addInitialValue(start, i, j, 1);
+								addInitialValue(sudoku, i, j, 1);
 						}
 					}
 				display(sudoku);
 				//returns same board
 				break;
 			case 5:
-				empty++;
+				keepPlaying++;
 				break;
 			default:
 				printf("Invalid input.\n");
